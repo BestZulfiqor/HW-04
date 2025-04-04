07326a428ed1b85f3e93cd338db9087bc0cad40c
Alimamatov Zulfiqor <152498839+BestZulfiqor@users.noreply.github.com>
2025-04-04 15:51:32 +0500

    Update Program.cs

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -8,6 +8,10 @@ var builder = WebApplication.CreateBuilder(args);
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddControllers();
 builder.Services.AddScoped<IStudentService, StudentService>();
+builder.Services.AddScoped<ICourseService, CourseService>();
+builder.Services.AddScoped<IGroupService, GroupService>();
+builder.Services.AddScoped<IStudentGroupService, StudentGroupService>();
+builder.Services.AddScoped<IAddressService, AddressService>();
 builder.Services.AddDbContext<DataContext>(t => t.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 // Добавьте регистрацию Swagger
 builder.Services.AddSwaggerGen(c =>
